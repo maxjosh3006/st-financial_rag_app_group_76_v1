9632a943b6a7f27e763c944b07950738918128d9
amit77k <amit.cusat.cs@gmail.com>
2025-03-13 11:17:24 +0530

    Update st-financial_rag_app_group_76_v1.py

--- st-financial_rag_app_group_76_v1.py
+++ st-financial_rag_app_group_76_v1.py
@@ -12,7 +12,7 @@ import nltk
 # ✅ Ensure NLTK's Punkt tokenizer is available
 nltk.data.path.clear()  # Clear existing paths
 nltk.download('punkt', download_dir='/workspaces/appuser/nltk_data')  # Re-download to a known directory
-
+nltk.data.path.append('/workspaces/appuser/nltk_data')
 
 from nltk.tokenize import sent_tokenize
 
